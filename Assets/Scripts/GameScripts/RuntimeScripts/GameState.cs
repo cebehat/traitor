dfe4622b9a0dbde384c463cf4e3b4f052099ad22
cebt22 <46456253+cebt22@users.noreply.github.com>
2021-09-29 10:50:22 +0200

    ability to connect to ip, needs test

--- Assets/Scripts/GameScripts/RuntimeScripts/GameState.cs
+++ Assets/Scripts/GameScripts/RuntimeScripts/GameState.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UNET;
 using UnityEngine;
 
 namespace Cebt
 {
     public class GameState : NetworkBehaviour
-    {   
+    {
+        private static string ip = "127.0.0.1";
+
         void OnGUI()
         {
             GUILayout.BeginArea(new Rect(10, 10, 300, 300));
@@ -24,9 +27,19 @@ namespace Cebt
 
         static void StartButtons()
         {
-            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
-            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
-            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+            ip = GUILayout.TextField(ip);
+            if (GUILayout.Button("Host"))
+            {
+                NetworkManager.Singleton.StartHost();
+            }
+            if (GUILayout.Button("Client")) {
+                NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = ip;
+                NetworkManager.Singleton.StartClient(); 
+            }
+            if (GUILayout.Button("Server")) 
+            {
+                NetworkManager.Singleton.StartServer();
+            }
         }
 
         static void StatusLabels()
@@ -37,6 +50,18 @@ namespace Cebt
             GUILayout.Label("Transport: " +
                 NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
             GUILayout.Label("Mode: " + mode);
+
+            if (GUILayout.Button("Disconnect")) Disconnect();
+        }
+
+        public static void Disconnect()
+        {
+            NetworkManager.Singleton.Shutdown();
+        }
+
+        void OnDestroy()
+        {
+            //Disconnect();
         }
     }
 }
